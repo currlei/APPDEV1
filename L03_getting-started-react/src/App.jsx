60e64956853e6b9936749571de146bb11c0abdd9
Lorein Manluctao <loreinmanluctao30@gmail.com>
2026-07-18 15:06:09 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+
+  return (
+    <>
+    <h1>ABOUT ME</h1>
+    <h2>Name: Lorein Fernandez Manluctao</h2>
+    <h2>Course & Year: Bachelor of Science in Information Systems 3</h2>
+    <h2>One fun fact about me: I love squishies and I love to fidget trade</h2>
+    <h2>What I learned so far in this course:</h2>
+    <ul>
+      <li>I learned how does Information Systems work, the serve as a bridge between technology and business</li>
+      <li>I learned how to the fundamentals of creating a web application and its core concepts</li>
+      <li>I learned how to be a good team member and communicate effectively</li>
+      <li>I learned how to manage and lead projects effectively</li>
+    </ul>
+
+
+    </>
+  )
+}
+
+export default App
